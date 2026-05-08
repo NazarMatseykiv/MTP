6aa240d7a48704d2f35536c8e2333a11b26d3606
Nazar <nmatseykiv@gmail.com>
2026-05-08 14:46:10 +0300

    lab8

---
+++ ProductApp/Controllers/EmployeeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductApp.Data;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            return await _context.Employee.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> AddEmployee(Employee employee)
+        {
+            _context.Employee.Add(employee);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEmployee(int id, Employee employee)
+        {
+            if (id != employee.Id)
+                return BadRequest();
+
+            _context.Entry(employee).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var employee = await _context.Employee.FindAsync(id);
+
+            if (employee == null)
+                return NotFound();
+
+            _context.Employee.Remove(employee);
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ ProductApp/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApp.Models;
+
+namespace ProductApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
